92982459751e27dbb58310b737a10b00fe7ee36b
robertotrapletti <roberto.trapletti@gmail.com>
2018-10-15 18:33:36 +0200

    init

---
+++ Reco/Reco.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+
+namespace Reco
+{
+    public static class Reco
+    {
+        public static bool init(this String i) {
+            return true;
+        }
+    }
+}
